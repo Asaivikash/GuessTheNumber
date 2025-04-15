95d04c98197c39c4c98c07673f7c38b329d82dc6
A Sai Vikas <111242832+Asaivikash@users.noreply.github.com>
2025-04-15 17:04:16 +0530

    Update App.jsx

--- guess-number-game/src/App.jsx
+++ guess-number-game/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <video autoPlay loop muted className="background-video">
-        <source src="/space.mp4" type="video/mp4" />
+        <source src="./space.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="overlay">
